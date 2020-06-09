5175cad4c8574a56c46f2331c9575b34018103b2
edionu97 <onu_edy@yahoo.com>
2020-06-09 15:31:05 +0300

    added server run batch file

--- JMS/Server/run.bat
+++ JMS/Server/run.bat
@@ -1,3 +1,7 @@
 @echo off
-C:\PROGRA~1\Java\jdk-11\bin\java.exe -jar C:\Users\Eduard\Desktop\Master\WSMT\MOM\JMS\Clients\build\libs\JmsClient.jar
+
+start cmd.exe /k C:\PROGRA~1\Java\jdk-11\bin\java.exe -jar C:\Users\Eduard\Desktop\Master\WSMT\MOM\JMS\Server\build\libs\JmsServer.jar
+
+cd ../../Apachemq/apache-activemq-5.15.2/bin && activemq start
+
 @pause 
